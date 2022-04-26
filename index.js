f05484c4a19934b7988e04316408a81e6397ad65
Yunus Hoshmand <yunus.hoshmandit@gmail.com>
2022-04-26 07:48:50 +0430

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. Using console.log()
+console.log("Hello World");
+// console.log() is used in debugging the code.
+
+// 2. Using alert()
+alert("Hello, World!");
+// // The alert() method displays an alert box over the current window with the specified message.
+
+// 3. Using document.write()
+document.write("Hello, World!");
+// // document.write() is used when you want to print the content to the HTML document.
